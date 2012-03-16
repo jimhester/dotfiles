f77a7704e0dd30ece9c8e23e171a081ea18642c7
Jim Hester <james.f.hester@gmail.com>
2012-03-16 10:44:00 -0400

    Add softlink.sh to set up the soft links

---
+++ softlink.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/dotfiles/zsh/zshenv ~/.zshenv
+ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -s ~/dotfiles/colemak ~/.colemak
