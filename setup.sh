7179f556da767787bb6cb4b174372fd612d507ce
Jim Hester <james.f.hester@gmail.com>
2012-10-03 11:36:59 -0400

    Rename softline.sh to setup.sh, add backup function
    backup function simply deletes any links, or renames the file to .bak if
    it is a regular file
    
    also add git default information in this setup file

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function backup(){
+  if [[ -h $1 ]]; then
+    rm $1
+  elif [[ -f $1 ]]; then
+    mv $1 $1.bak
+  fi
+}
+backup ~/.vim;ln -s ~/dotfiles/vim ~/.vim
+backup ~/.vimrc;ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+backup ~/.zshenv;ln -s ~/dotfiles/zsh/zshenv ~/.zshenv
+backup ~/.zshrc;ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+backup ~/.tmux.conf;ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+backup ~/.inputrc;ln -s ~/dotfiles/input/inputrc ~/.inputrc
+backup ~/.Renviron;ln -s ~/dotfiles/R/Renviron ~/.Renviron
+
+git config --global core.excludesfile ~/dotfiles/.gitignore
+git config --global user.name "Jim Hester"
+git config --global user.email "james.f.hester@gmail.com"
--- softlink.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ln -s ~/dotfiles/vim ~/.vim
-ln -s ~/dotfiles/vim/vimrc ~/.vimrc
-ln -s ~/dotfiles/zsh/zshenv ~/.zshenv
-ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
-ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/input/inputrc ~/.inputrc
-ln -s ~/dotfiles/R/Renviron ~/.Renviron
