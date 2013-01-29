ab915fe058a3bb85dfa4cf6d32788db86276cd3a
Jim Hester <james.f.hester@gmail.com>
2013-01-29 09:36:50 -0500

    Update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,10 +1,14 @@
 #htop crashes if term is not xterm
 alias htop="TERM=xterm htop"
+alias l='ls -lhtcr --color=auto'
 alias lc='ls -lhtcr --color=auto'
 alias lk='ls -lhSr' # sort by size
 alias la='ls -ac --color=auto'
 alias ll='ls -lah --color=auto'
 alias lsd='ls -d */'
+alias le='less'
+alias ht='htop'
+alias pe='perl'
 alias g="grep --color=always"
 alias gi="grep -i --color=always"
 alias cp='cp -a'
