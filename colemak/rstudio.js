e8dae4d2b71cedcbcad5f67752617c1368d86777
Jim Hester <james.f.hester@gmail.com>
2017-12-06 15:07:10 -0500

    Add rstudio configuration

---
+++ colemak/rstudio.js
@@ -0,0 +1,20 @@
+// rotate some keys about to get qwerty "hjkl" back for movement
+var vim = require("ace/keyboard/vim").CodeMirror.Vim
+vim.mapCommand({keys:"n",type:"motion",motion:"moveByLines",motionArgs:{forward:!0,linewise:!0}})
+vim.mapCommand({keys:"e",type:"motion",motion:"moveByLines",motionArgs:{forward:!1,linewise:!0}})
+vim.mapCommand({keys:"l",type:"motion",motion:"moveByCharacters",motionArgs:{forward:!0}})
+
+// move these keys to their qwerty positions because they are
+// in the way of hjkl (and E for J)
+vim.mapCommand({keys:"k",type:"motion",motion:"findNext",motionArgs:{forward:!0,toJumplist:!0}})
+vim.mapCommand({keys:"K",type:"motion",motion:"findNext",motionArgs:{forward:!1,toJumplist:!0}})
+vim.mapCommand({keys:"u",type:"action",action:"enterInsertMode",isEdit:!0,actionArgs:{insertAt:"inplace"},context:"normal"})
+vim.mapCommand({keys:"U",type:"action",action:"enterInsertMode",isEdit:!0,actionArgs:{insertAt:"firstNonBlank"},context:"normal"})
+vim.mapCommand({keys:"l",type:"action",action:"undo",context:"normal"})
+vim.mapCommand({keys:"L",type:"operator",operator:"changeCase",operatorArgs:{toLower:!1},context:"visual",isEdit:!0})
+vim.mapCommand({keys:"N",type:"action",action:"joinLines",isEdit:!0})
+vim.mapCommand({keys:"I",type:"motion",motion:"moveToBottomLine",motionArgs:{linewise:!0,toJumplist:!0}})
+
+// this is the only key that isn't in qwerty or colemak position
+vim.mapCommand({keys:"j",type:"motion",motion:"moveByWords",motionArgs:{forward:!0,wordEnd:!0,inclusive:!0}})
+vim.mapCommand({keys:"J",type:"motion",motion:"moveByWords",motionArgs:{forward:!0,wordEnd:!0,bigWord:!0,inclusive:!0}})
